338bbdaa3fb24a222a36c9ddd43f646091696868
Jagtesh Chadha <jagtesh@gmail.com>
2019-03-14 06:35:57 +0000

    feat(formtable): renamed files, added a new example

---
+++ src/FormTable.elm
@@ -0,0 +1,36 @@
+module Main exposing (..)
+
+import Browser exposing (sandbox)
+import Html exposing (input, div, ul, li, button, text)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onClick, onInput)
+
+initModel = {
+  inputText = "",
+  names = [] }
+
+type Msg = Change String | AddName | RemoveName String
+
+update msg model =
+  case msg of
+    Change newInputText ->
+      { model | inputText = newInputText }
+    AddName ->
+      { model | names = model.names ++ [model.inputText] }
+    RemoveName name ->
+      { model | names = List.filter (\n -> n /= name) model.names }
+
+    
+
+view model =
+  div []
+    [ input [ placeholder "Name", value model.inputText, onInput Change ] []
+    , button [ onClick AddName ] [ text "Add" ]
+    , ul [] (List.map (\n -> viewItem n) model.names)
+    ]
+
+--viewItem : String -> List Html
+viewItem n = li [] [ text n
+  , button [ onClick (RemoveName n) ] [ text "-" ] ]
+
+main = sandbox { init = initModel, view = view, update = update }
