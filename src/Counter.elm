716e4f3dd2696403dd6f7c8704bc4000d5bbb16f
Jagtesh Chadha <jagtesh@gmail.com>
2019-03-14 06:52:47 +0000

    chore: formatted with elm-format

--- src/Counter.elm
+++ src/Counter.elm
@@ -1,30 +1,42 @@
-module Main exposing (..)
+module Main exposing (Msg(..), initModel, main, update, view)
 
 import Browser exposing (sandbox)
 import Html exposing (button, div, text)
 import Html.Events exposing (onClick)
 
-initModel =
-  { total = 0 
-  , lastTotal = 0 }
 
-type Msg = Increment | Decrement | Clear
+initModel =
+    { total = 0
+    , lastTotal = 0
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+    | Clear
+
 
 update msg model =
-  case msg of
-    Clear ->
-      { model | total = model.lastTotal, lastTotal = 0 }
-    Increment ->
-      { model | total = model.total + 1, lastTotal = model.total }
-    Decrement ->
-      { model | total = model.total - 1, lastTotal = model.total }
+    case msg of
+        Clear ->
+            { model | total = model.lastTotal, lastTotal = 0 }
+
+        Increment ->
+            { model | total = model.total + 1, lastTotal = model.total }
+
+        Decrement ->
+            { model | total = model.total - 1, lastTotal = model.total }
+
 
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model.total) ]
-    , button [ onClick Increment ] [ text "+" ]
-    , button [ onClick Clear ] [ text "Undo -> Clear" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model.total) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Clear ] [ text "Undo -> Clear" ]
+        ]
 
-main = sandbox { init = initModel, view = view, update = update }
+
+main =
+    sandbox { init = initModel, view = view, update = update }
--- src/FormTable.elm
+++ src/FormTable.elm
@@ -1,36 +1,53 @@
-module Main exposing (..)
+module Main exposing (Msg(..), initModel, main, update, view, viewItem)
 
 import Browser exposing (sandbox)
-import Html exposing (input, div, ul, li, button, text)
+import Html exposing (button, div, input, li, text, ul)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onClick, onInput)
 
-initModel = {
-  inputText = "",
-  names = [] }
 
-type Msg = Change String | AddName | RemoveName String
+initModel =
+    { inputText = ""
+    , names = []
+    }
+
+
+type Msg
+    = Change String
+    | AddName
+    | RemoveName String
+
 
 update msg model =
-  case msg of
-    Change newInputText ->
-      { model | inputText = newInputText }
-    AddName ->
-      { model | names = model.names ++ [model.inputText] }
-    RemoveName name ->
-      { model | names = List.filter (\n -> n /= name) model.names }
+    case msg of
+        Change newInputText ->
+            { model | inputText = newInputText }
 
-    
+        AddName ->
+            { model | names = model.names ++ [ model.inputText ] }
+
+        RemoveName name ->
+            { model | names = List.filter (\n -> n /= name) model.names }
+
 
 view model =
-  div []
-    [ input [ placeholder "Name", value model.inputText, onInput Change ] []
-    , button [ onClick AddName ] [ text "Add" ]
-    , ul [] (List.map (\n -> viewItem n) model.names)
-    ]
+    div []
+        [ input [ placeholder "Name", value model.inputText, onInput Change ] []
+        , button [ onClick AddName ] [ text "Add" ]
+        , ul [] (List.map (\n -> viewItem n) model.names)
+        ]
+
+
 
 --viewItem : String -> List Html
-viewItem n = li [] [ text n
-  , button [ onClick (RemoveName n) ] [ text "-" ] ]
 
-main = sandbox { init = initModel, view = view, update = update }
+
+viewItem n =
+    li []
+        [ text n
+        , button [ onClick (RemoveName n) ] [ text "-" ]
+        ]
+
+
+main =
+    sandbox { init = initModel, view = view, update = update }
